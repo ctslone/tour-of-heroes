7f71b7702e6408c98148c27c03ba4291ce39a922
Charlie <ctslone27@gmail.com>
2020-08-03 21:41:41 -0400

    injecting hero service in the heroes class component to fetch the mock heroes data and display normally.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,21 +9,27 @@ import { HeroService } from '../hero.service'
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // }
 
   heroes: Hero[];
 
   selectedHero: Hero;
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  constructor() { }
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes()
+  }
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.getHeroes()
   }
 
 }
